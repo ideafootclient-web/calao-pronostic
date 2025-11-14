a62633bd8590e6942ea4f4999929819e445564e1
ideafootclient-web <ideafootclient@gmail.com>
2025-11-14 21:56:15 +0100

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,1 +1,13 @@
-export default { images: { domains: [] } }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
+  },
+};
+
+export default nextConfig;
